6ccb4acf25dd678bb4923e24593cd180249c669b
Clupai8o0 <clupaio4@gmail.com>
2022-08-02 11:17:36 +0300

    transitions for circles

--- js/month.js
+++ js/month.js
@@ -1,19 +1,22 @@
 const monthSvg = d3.select("svg.month");
 
 //* Adding a range limiter
-const radiusScale = d3.scaleSqrt()
-  .domain([0, 30000])
-  .range([0, 50])
+const radiusScale = d3.scaleSqrt().domain([0, 30000]).range([0, 50]);
 
 //* Creating the bars
 monthSvg
-	.selectAll("circle") 
+	.selectAll("circle")
 	.data(monthData)
 	.enter()
 	.append("circle")
 	.attr("cx", (_, i) => (i % 7) * 120 + 60)
 	.attr("cy", (_, i) => Math.floor(i / 7) * 100 + 60)
-	.attr("r", (d) => radiusScale(d))
+	.attr("r", 0)
+	.transition()
+	.duration(1000)
+	.delay((_, i) => i * 20 + 500)
+  .ease(d3.easeCubicOut)
+	.attr("r", (d) => radiusScale(d));
 
 //* Adding text
 // monthSvg
