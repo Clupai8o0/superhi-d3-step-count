3f56f1a758a7ce4915924f4042729916e4371b0e
Clupai8o0 <clupaio4@gmail.com>
2022-08-02 10:58:07 +0300

    adding somewhat of a basic bars

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const todaySvg = d3.select("svg");
 
 todaySvg
-  .selectAll("rect") // selecting if there are any rects
+	.selectAll("rect") // selecting if there are any rects
 	.data(todayData)
 	.enter()
 	.append("rect") // adding the rects based on the amount of data
-	.attr("width", (data) => data)
-	.attr("height", 10)
-	.attr("y", (_, i) => i * 15);
+	.attr("x", (_, i) => i * 36)
+	.attr("y", (d) => 112 - d / 20)
+	.attr("width", 24)
+	.attr("height", (d) => d / 20);
