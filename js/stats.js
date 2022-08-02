6d06ca862f3512a6f56666d04d104136fbf69ccf
Clupai8o0 <clupaio4@gmail.com>
2022-08-02 11:57:42 +0300

    final

--- js/stats.js
+++ js/stats.js
@@ -5,3 +5,4 @@ d3.select("p.average-day").text(statsFormat(d3.mean(monthData)) + " steps");
 d3.select("p.best-day").text(statsFormat(d3.max(monthData)) + " steps");
 d3.select("p.total-month").text(statsFormat(d3.sum(monthData)) + " steps");
 d3.select("p.total-day").text(statsFormat(d3.sum(todayData)) + " steps");
+d3.select("p.biggest-difference").text(statsFormat(d3.max(monthData) - d3.min(monthData)) + " steps");
