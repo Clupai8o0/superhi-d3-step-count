9c226dcb4dc2aa9bbf3dfe16784b680d4899ea93
Clupai8o0 <clupaio4@gmail.com>
2022-08-02 10:59:40 +0300

    adding a scale

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const todaySvg = d3.select("svg");
 
+const barScale = d3.scaleLinear()
+	.domain([0, 2000])
+	.range([1, 112])
+
 todaySvg
 	.selectAll("rect") // selecting if there are any rects
 	.data(todayData)
 	.enter()
 	.append("rect") // adding the rects based on the amount of data
 	.attr("x", (_, i) => i * 36)
-	.attr("y", (d) => 112 - d / 20)
+	.attr("y", (d) => 112 - barScale(d))
 	.attr("width", 24)
-	.attr("height", (d) => d / 20);
+	.attr("height", (d) => barScale(d));
