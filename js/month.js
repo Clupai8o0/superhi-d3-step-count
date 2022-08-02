88bdc3b287073b9b338bd9b9c9b92d4593cdab62
Clupai8o0 <clupaio4@gmail.com>
2022-08-02 11:39:33 +0300

    adding groups for circles

--- js/month.js
+++ js/month.js
@@ -3,19 +3,28 @@ const monthSvg = d3.select("svg.month");
 //* Adding a range limiter
 const radiusScale = d3.scaleSqrt().domain([0, 30000]).range([0, 50]);
 
-//* Creating the bars
-monthSvg
-	.selectAll("circle")
+//* month groups
+const monthGroups = monthSvg
+	.selectAll("g")
 	.data(monthData)
 	.enter()
+	.append("g")
+	.attr("transform", (_, i) => {
+		const x = (i % 7) * 120 + 60,
+			y = Math.floor(i / 7) * 150 + 60;
+		return `translate(${x}, ${y})`;
+	});
+
+//* Creating the bars
+monthGroups
 	.append("circle")
-	.attr("cx", (_, i) => (i % 7) * 120 + 60)
-	.attr("cy", (_, i) => Math.floor(i / 7) * 100 + 60)
+	.attr("cx", 0)
+	.attr("cy", 0)
 	.attr("r", 0)
 	.transition()
 	.duration(250)
 	.delay((_, i) => i * 20 + 500)
-  .ease(d3.easeCubicOut)
+	.ease(d3.easeCubicOut)
 	.attr("r", (d) => radiusScale(d));
 
 //* Adding text
